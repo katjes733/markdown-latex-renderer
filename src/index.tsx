c7a3c35e4c4a4efc5c4ea521e9699915bf85e519
Martin Macecek <katjes733@gmx.net>
2025-03-08 16:34:04 -0700

    fix: type export inconsistency (#4)
    * fix: incorrectly exported old type

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import MarkdownLatexRenderer from "./factory/RcbPluginFactory";
 import { MarkdownLatexRendererBlock } from "./types/MarkdownLatexRendererBlock";
 import { PluginConfig } from "./types/PluginConfig";
 
-export type {
-    MarkdownLatexRendererBlock as MarkdownRendererBlock,
-    PluginConfig,
-};
+export type { MarkdownLatexRendererBlock, PluginConfig };
 
 export default MarkdownLatexRenderer;
